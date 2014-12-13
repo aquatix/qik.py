a03a34f80e977c4015aecc9d98c8be5c5419c306
Michiel Scholten <michiel@aquariusoft.org>
2014-12-13 13:05:13 +0100

    Better looking import lines

--- qik_website/views/general.py
+++ qik_website/views/general.py
@@ -1,5 +1,14 @@
-from flask import Blueprint, render_template, session, redirect, url_for, \
-    request, flash, g, jsonify, abort
+from flask import (
+    Blueprint,
+    render_template,
+    session,
+    redirect,
+    url_for,
+    request,
+    flash,
+    g,
+    jsonify,
+    abort)
 
 
 mod = Blueprint('general', __name__)
